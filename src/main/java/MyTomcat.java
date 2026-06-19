a05620066ee4279425f1e3e7759a4481f95347e4
yewongit <yunyeowon51@gmail.com>
2026-06-20 00:03:41 +0900

    feat: 2단계 - Socket InputStream 기반의 HTTP 요청 메시지 파싱 및 콘솔 출력

--- src/main/java/MyTomcat.java
+++ src/main/java/MyTomcat.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,14 +9,25 @@ public class MyTomcat {
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
             // "ServerSocket serverSocket = new ServerSocket(8080)"은 자바 OS에게 8080번 포트 권한을 요청하는 코드
             // OS 커널은 8080번 포트를 다른 프로그램이 쓰지 못하도록 락을 걸어 선점하고, 이 포트로 들어오는 TCP 연결 요청(SYN 패킷)을 임시로 보관할 '대기 큐'를 컴퓨터 메모리에 만듬
-            System.out.println("1단계: 서버가 8080 포트에서 대기 중입니다.");
+            System.out.println("2단계: 8080 포트에서 요청을 기다림");
 
             // 브라우저가 접속할 때까지 여기서 프로그램이 잠시 멈춤 (Blocking)
             // 브라우저 주소창에 localhost:8080을 입력하면, 브라우저와 OS 커널 간에 TCP 3-Way Handshake가 일어남
             // OS가 Handshake가 끝나면 큐에 집어넣음
             // accept()는 큐를 지켜보다가 요청이 들어오는 순간 Blocking을 풀고 요청을 자바의 Socket 객체로 만들어서 반환
             Socket clientSocket = serverSocket.accept();
-            System.out.println("브라우저가 서버에 접속했습니다.");
+
+            // 브라우저가 네트워크를 통해 보내는 바이트 데이터 읽기
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String line;
+
+            // HTTP 프로토콜 규격상, 브라우저가 보내는 요청 메시지는 헤더 뒤에 빈줄을 한 줄 넣음 -> "!line.isEmpty()" 조건으로 Blocking 막음
+            while((line = reader.readLine()) != null && !line.isEmpty()) {
+                System.out.println(line);
+            }
+
+            System.out.println("요청을 모두 읽었으므로 프로그램을 종료");
         } catch (Exception e) {
             e.printStackTrace();
         }
